9ae0e2a4747101bc0b664df547570606cf9fcc83
Shirley Xiao <s2xiao@uwaterloo.ca>
2025-03-23 22:40:16 -0400

    ownable building declaration -shirley

---
+++ ownable-impl.cc
@@ -0,0 +1,3 @@
+/*Implementation File for ownable properties*/
+import ownable;
+
---
+++ ownable.cc
@@ -0,0 +1,25 @@
+//OWNABLE is a class that defines the general structure for a property that is ownable
+//OWNABLE is an Abstract Base Class
+
+export module ownable;
+import Building;
+import <string>;
+
+using namespace std;
+
+export class Ownable: public Building {
+
+    int propertyCost;
+    bool isMortgaged;
+
+    public:
+        Player *owner; //pointer to the owner, with nullptr denoting no ownership status
+        
+        //add ctor
+
+        string getPropName();
+        int getCost() const;
+        Player getOwner() const;
+        bool getMortgageState() const;
+        char getBType() const;
+};
