b14e70b5a369683f6f7b5b8553df4f86dfea1cd8
shirleyxxiao <shirleyxiaocoop@gmail.com>
2025-03-24 21:29:14 -0400

    residence rent calculation - shirley

--- residence-impl.cc
+++ residence-impl.cc
@@ -6,6 +6,21 @@ using namespace std;
 
 Residence::Residence(string name, size_t blockPosition, Player *owner): Ownable{name, blockPosition, true, owner, 200} {}
 
-/*
-implement int calcResCost(int ownedNum);
-*/
+
+int Residence::calcResCost(int ownedNum) {
+    int feeOwed = 0;
+    
+    if (ownedNum == 1) {
+        return 25;
+    } else if (ownedNum == 2) {
+        return 50;
+    } else if (ownedNum == 3) {
+        return 100;
+    } else if (ownedNum == 4) {
+        return 200;
+    }
+
+    return feeOwed;
+}
+
+
